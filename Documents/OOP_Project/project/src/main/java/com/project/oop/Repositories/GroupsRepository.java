9bcb302093e784341bff40d8a8330d77fa103609
Kamal <gamrikamal@gmail.com>
2024-05-22 23:38:26 +0500

    Remove files

--- Documents/OOP_Project/project/src/main/java/com/project/oop/Repositories/GroupsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.oop.Repositories;
-
-import com.project.oop.Tables.Groups;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GroupsRepository extends JpaRepository<Groups, Integer> {
-}
--- Documents/OOP_Project/project/src/main/java/com/project/oop/Repositories/TeachersRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.oop.Repositories;
-
-import com.project.oop.Tables.Teachers;
-import org.hibernate.id.IntegralDataTypeHolder;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeachersRepository extends JpaRepository<Teachers, Integer> {
-}
