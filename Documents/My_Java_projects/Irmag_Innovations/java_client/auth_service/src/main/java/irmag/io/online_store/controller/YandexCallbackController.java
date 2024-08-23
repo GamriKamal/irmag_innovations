4222f84c725bc1ef0705953305dd7b335b6077d0
GamriKamal <124342926+GamriKamal@users.noreply.github.com>
2024-08-23 12:08:48 +0500

    Delete Documents/My_Java_projects/Irmag_Innovations/java_client directory

--- Documents/My_Java_projects/Irmag_Innovations/java_client/auth_service/src/main/java/irmag/io/online_store/controller/YandexCallbackController.java
+++
@@ -1,9 +0,0 @@
-package irmag.io.online_store.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class YandexCallbackController {
-
-}
-
--- Documents/My_Java_projects/Irmag_Innovations/java_client/auth_service/src/main/java/irmag/io/online_store/domain/model/Role.java
+++
@@ -1,6 +0,0 @@
-package irmag.io.online_store.domain.model;
-
-public enum Role {
-    ROLE_USER,
-    ROLE_ADMIN
-}
